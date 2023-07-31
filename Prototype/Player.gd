e393e7f97a41be33554c49b396c0694efd5e9fb8
Bouzenad <bouzenadabdelhak@gmail.com>
2023-07-31 20:48:06 +0100

    A

--- Prototype/Player.gd
+++ Prototype/Player.gd
@@ -5,12 +5,8 @@ var degrees = 0
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
 
-
-#head.rotate_y(-event.relative.x * SENSITIVITY)
-
-
 func _on_detect_left_mouse_entered() -> void:
 	while degrees > -90:
 		head.rotate_y(-1)
 		degrees -= 1
-		await(get_tree().create_timer(0.01),"timeout")
+		await get_tree().create_timer(0.01).timeout
